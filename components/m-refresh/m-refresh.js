f795a25625ea8f41a3656879c1fb03d3a09d4028
lushaojie <1009636982@qq.com>
2020-03-01 16:53:41 +0800

    自定义下拉刷新2.0

--- components/m-refresh/m-refresh.js
+++ components/m-refresh/m-refresh.js
@@ -1,26 +1,42 @@
-
+// *: 必须在组件外部加一层外框并且固定宽高
 const dampNum = 0.95; // 衰减指数
 const transitionExp = 0.3; // 回弹动画的时长
 
+const refreshTipHeight = 50; // 刷新提示文字或动画的dom高度，初始化的偏移量回取此值的负数
+
 let lastY = 0;  // 存储手指按下时的y轴位置
 let rubNum = 1; // 摩擦力
-let refreshState = 0; // 刷新状态： 0-初始状态，1-正在刷新,
 
 Component({
     /**
      * 组件的属性列表
      */
     properties: {
-
+        // 触发下拉刷新的滑动距离(大于0 ，因为组件初始会在y轴便宜 offsetHeight 值，
+        // 所以下拉刷新的实际距离是：downRefreshDistance + Math.abs(offsetHeight) )
+        downRefreshDistance : {
+            type : Number,
+            value : 0
+        },
+        // 是否启用刷新功能 false 将关闭所有事件监听
+        isListenRefresh : {
+            type : Boolean,
+            value : true
+        }
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-        offsetHeight : -50,
-        pullDownTip: "下拉刷新",
-        tstNum : 0
+        refreshState: 0, // 刷新状态： 0-初始状态，1,松开刷新，2-正在刷新,
+        _refreshTipHeight: refreshTipHeight,
+        offsetHeight: -refreshTipHeight,  // 
+        pullDownTip: ["下拉刷新", "松开刷新","正在刷新"],
+        tstNum: 0  // 真正起作用的回弹动画的时长
+    },
+    attached(){
+
     },
 
     /**
@@ -29,7 +45,10 @@ Component({
     methods: {
         //触摸开始
         handletouchstart: function (event) {
-            if (refreshState == 1){
+            if (!this.data.isListenRefresh){
+                return;
+            }
+            if (this.data.refreshState == 2){
                 console.log('刷新中...')
                 return;
             }
@@ -38,23 +57,37 @@ Component({
         //触摸移动
         handletouchmove: function (event) {
             // console.log(event);
+            if (!this.data.isListenRefresh) {
+                return;
+            }
 
-            if (refreshState == 1) {
+            if (this.data.refreshState == 2) {
                 return;
             }
 
             let moveY = event.touches[0].clientY;
             let offsetY = moveY - lastY;
 
-            rubNum *= dampNum; // 衰减摩擦力
-            // console.log(rubNum,'===');
-            offsetY *= rubNum; // 摩擦偏移量
+            // console.log(offsetY,'===');
+            if (offsetY < 0){
+                // 当offsetY小于0时说明是上拉操作，暂不做上拉加载功能
+                // console.log("上拉加载...");
+                return;
+            }
 
-            let { offsetHeight } = this.data;
+            // 当偏移量小于1时不做摩擦处理
+            if (offsetY > 1){
+                rubNum *= dampNum; // 衰减摩擦力
+                // console.log(rubNum,'===');
+                offsetY *= rubNum; // 摩擦偏移量
+            }
 
+            let { offsetHeight, refreshState, downRefreshDistance } = this.data;
+            offsetHeight = offsetHeight + offsetY;
             // console.log(offsetY,'===');
             this.setData({
-                offsetHeight: offsetHeight + offsetY
+                offsetHeight,
+                refreshState: offsetHeight > downRefreshDistance ? 1 : refreshState
             });
 
             // console.log(this.data.offsetHeight)
@@ -65,27 +98,30 @@ Component({
         handletouchend: function (event) {
             // console.log(event);
             // console.log(this.data.offsetHeight,'end---');
-
-            if (refreshState == 1) {
+            if (!this.data.isListenRefresh) {
                 return;
             }
 
+            if (this.data.refreshState == 2) {
+                return;
+            }
+            let { offsetHeight, downRefreshDistance } = this.data;
+
+            // 开启回弹动画
             this.setData({
                 tstNum: transitionExp
             });
 
-            if ( this.data.offsetHeight > 0 ){
-
-                refreshState = 1;
+            // 一开始的偏移量默认是-50
+            if ( offsetHeight > downRefreshDistance ){
 
                 this.setData({
                     offsetHeight: 0,
-                    pullDownTip : "正在刷新"
+                    refreshState : 2
                 })
 
-                setTimeout( () => {
-                    this.stopPullRefresh();
-                },2000 )
+                // 发送正在刷新事件
+                this.triggerEvent('pullDownRefreshEvent');
 
             }else{
                 this.stopPullRefresh();
@@ -100,13 +136,13 @@ Component({
         //停止刷新
         stopPullRefresh() {
             this.setData({
-                offsetHeight: -50,
-                pullDownTip: "下拉刷新"
+                offsetHeight: -refreshTipHeight
             },() => {
+                // 关闭回弹动画
                 this.setData({
-                    tstNum: 0
+                    tstNum: 0,
+                    refreshState : 0
                 });
-                refreshState = 0;
             })
 
         }
--- pages/home/home.js
+++ pages/home/home.js
@@ -6,14 +6,26 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        oRefresh : null,
+        isFullCover : false
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+        this.data.oRefresh = this.selectComponent("#refresh_wrap_id");
     },
+    onPullDownRefreshEvent(){
+        this.setData({
+            isFullCover : true
+        });
+        setTimeout(() => {
+            this.data.oRefresh.stopPullRefresh();
+            this.setData({
+                isFullCover: false
+            });
+        }, 2000)
+    }
     
 })
